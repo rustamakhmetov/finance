e399532d5e6b469d60684dbad33ae8962163c654
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-08-29 16:09:52 +0500

    Add dashboard

---
+++ app/assets/javascripts/components/finance_application.js.jsx
@@ -0,0 +1,14 @@
+var FinanceApplication = React.createClass({
+    render: function() {
+        return(
+            <div className="container">
+                <div className="jumbotron">
+                    <h1>Finance</h1>
+                </div>
+                <div className="row">
+                    Table
+                </div>
+            </div>
+        )
+    }
+});
